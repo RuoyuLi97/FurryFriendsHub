cb40b55e867a5e85fb24210f237140b28c485d94
RuoyuLi97 <ruoyuli@andrew.cmu.edu>
2024-12-01 18:05:15 -0800

    update FurryFriendsHubApplication.java

--- src/main/java/com/furryfriendshub/FurryFriendsHubApplication.java
+++ src/main/java/com/furryfriendshub/FurryFriendsHubApplication.java
@@ -1,13 +1,13 @@
 package com.furryfriendshub;
-import com.furryfriendshub.util.IDGenerator;
+
 import com.furryfriendshub.config.MongoDBConnection;
-import com.furryfriendshub.model.Admin;
-import com.furryfriendshub.model.AdoptionListing;
-import com.furryfriendshub.model.ForumPost;
-import com.furryfriendshub.model.Notification;
-import com.furryfriendshub.model.User;
+import com.mongodb.client.MongoDatabase;
 
 public class FurryFriendsHubApplication {
     public static void main(String[] args) {
+        // Attempt to get the MongoDB database
+        MongoDatabase database = MongoDBConnection.getDatabase();
+        // Close the connection when done
+        MongoDBConnection.closeConnection();
     }
 }
